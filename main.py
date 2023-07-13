1df59919e64aeb6b860b43c744985cff59c3e8ac
Zion Tai <2508164094@qq.com>
2023-07-13 15:47:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 #### CONFIG AREA #####
 
 project_id = ""
-pay_money = 12800 #真实应付价格
+pay_money = -1 #真实应付价格
 deviceId = "" #非必要
 buy_time = int(time.mktime(time.strptime("2099-12-31 23:59:59", "%Y-%m-%d %H:%M:%S")))#发售刷新开始时间点，在此时可以开始疯狂刷新开售prepare了，请注意修改
 cookie = ''
@@ -30,8 +30,8 @@ def get_ids(project_id):
     for i in range(len(tickets)):
         print("["+str(i)+"] "+tickets[i]["desc"]+" "+str(tickets[i]["price"]/100)+"元")
     sku_id = input("请输入票档序号：")
-    ids = str(screens[int(screen_id)]["id"])+" "+str(tickets[int(sku_id)]["id"])
-    print("[INFO]您的screen_id 和 sku_id 分别为："+ids)
+    ids = str(screens[int(screen_id)]["id"])+" "+str(tickets[int(sku_id)]["id"])+" "+str(tickets[int(sku_id)]["price"])
+    print("[INFO]您的screen_id 和 sku_id 和 pay_money 分别为："+ids)
     return ids
 
 def get_buyer_info():
@@ -129,11 +129,11 @@ if(__name__ == "__main__"):
         print("[WARNING] 未设置购票时间")
     if(cookie == ""):
         cookie = input("请输入cookie：")
-    if(project_id == "" or screen_id == "" or sku_id == ""):
+    if(project_id == "" or screen_id == "" or sku_id == "" or pay_money == -1):
         if(project_id == ""):
             project_id = input("请输入项目id：")
         ids = get_ids(project_id)
-        screen_id,sku_id = ids.split(" ")
+        screen_id,sku_id,pay_money = ids.split(" ")
     buyer_info = get_buyer_info()
     count = len(json.loads(buyer_info))
     token=get_token(screen_id,sku_id,project_id, count)
