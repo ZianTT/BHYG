25affe32d30c8320c2cbfd48eb9ebef533a25b2e
ZianTT <53261506+ZianTT@users.noreply.github.com>
2024-06-25 14:31:02 +0000

    feat: bigVip detect

--- globals.py
+++ globals.py
@@ -264,6 +264,8 @@ def load_config():
             user = user.json()
             if user["data"]["isLogin"]:
                 logger.success("用户 " + user["data"]["uname"] + " 登录成功")
+                if user["data"]["vipStatus"] == 1:
+                    logger.info(f"用户为大会员，距离到期还有{user['data']['vipDue']-user['data']['now']/1000/60/60/24:.2f}天")
                 import machineid
                 sentry_sdk.set_user(
                     {
